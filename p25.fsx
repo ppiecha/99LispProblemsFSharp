36366070be5abe39ded783b6433a6451101c913d
ppiecha <piotr.piecha@outlook.com>
2022-10-08 03:27:39 +0200

    new exercises

--- p25.fsx
+++ p25.fsx
@@ -6,3 +6,43 @@ Example:
 
 Hint: Use the solution of problem P23.
 *)
+
+let removeAt list index =
+    let rec loop list counter =
+        match list with
+        | [] -> []
+        | h :: t ->
+            if counter = index then
+                loop t (counter + 1)
+            else
+                h :: loop t (counter + 1)
+    loop list 1
+
+let rec randomRemove list count =
+    let rand = System.Random()
+    if count = 0 then
+        list
+    else
+        let truncated = removeAt list (rand.Next(List.length list) + 1) 
+        randomRemove truncated (count - 1)
+        
+let rec randomRemoveOne list =
+    let rand_index = System.Random().Next(List.length list)
+    let rand_value = List.item rand_index list
+    let truncated = removeAt list (rand_index + 1) 
+    rand_value, truncated        
+
+let randPerm list =
+    let rec get_single_elem list times =
+        let value, truncated = randomRemoveOne list
+        if times = 1 then [value] else value :: get_single_elem truncated (times - 1)
+    get_single_elem list (List.length list)
+
+randPerm ['a';'b';'c';'d';'e';'f']
+
+let randPerm' list =
+    let randList = let r = System.Random() in List.init (List.length list) (fun _ -> r.Next())
+    list |> List.zip randList |> List.sortBy fst |> List.map snd  
+    
+randPerm' ['a';'b';'c';'d';'e';'f']
+
---
+++ p26.fsx
@@ -0,0 +1,42 @@
+﻿(*
+P26 (**) Generate the combinations of K distinct objects chosen from the N elements of a list
+In how many ways can a committee of 3 be chosen from a group of 12 people? We all know that there are C(12,3) = 220 possibilities (C(N,K) denotes the well-known binomial coefficients). For pure mathematicians, this result may be great. But we want to really generate all the possibilities in a list.
+
+Example:
+* (combination 3 '(a b c d e f))
+((A B C) (A B D) (A B E) ... )
+*)
+
+let rec combinations n list =
+    match list, n with
+    | [], _ -> [[]]
+    | list, 1 -> [for x in list -> [x]]
+    | h :: t, n -> [for x in combinations (n - 1) t do
+                        yield h :: x
+                    if List.length t > n then
+                        yield! combinations n t
+                    else
+                        yield t]
+    
+    
+combinations 2 ['a'; 'b'; 'c']
+combinations 0 ['a'; 'b'; 'c']
+combinations 0 ['a'; 'b']
+combinations 0 ['a']
+
+let rec tails = function
+    | [] -> [[]]
+    | _ :: t as list -> list :: tails t
+
+tails ['a'; 'b'; 'c']
+
+let rec combinations' n list =
+    match list, n with
+    | _, 0 -> [[]]
+    | list, n -> [for tail in tails list do
+                     match tail with
+                     | [] -> ()
+                     | h :: t -> for combination in combinations' (n - 1) t do
+                                     yield h :: combination]
+
+combinations' 2 ['a'; 'b'; 'c']
